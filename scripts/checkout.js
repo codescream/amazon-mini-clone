acdf46baa9629f10ecde748f5ee08184869681fd
mark <ogilom@gmail.com>
2023-11-16 10:37:18 -0500

    reformat code

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -4,7 +4,6 @@ import { formatCurrency } from "./utils/money.js";
 import { deliveryOptions, convertDeliveryOptions } from "../data/deliveryOptions.js";
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
 
-let cartGenerated = '';
 let deliveryOptionsHtml;
 
 export function formatDate(numberOfDays) {
@@ -14,56 +13,86 @@ export function formatDate(numberOfDays) {
     .format('dddd, MMMM D');
 }
 
+function renderCheckOutPage() {
+  let cartGenerated = '';
 
-cart.forEach((cartItem, index) => {
+  cart.forEach((cartItem, index) => {
 
-  products.forEach((product) => {
-    if (product.id === cartItem.id) {
-      const { image, name, priceCents } = product;
-
-      cartGenerated +=`<div class="cart-item-container js-cart-item-container-${product.id}">
-            <div class="delivery-date js-delivery-date">
-              Delivery date: ${cartItem.deliveryOptionId ? convertDeliveryOptions(cartItem.deliveryOptionId) : ''}
-            </div>
-
-            <div class="cart-item-details-grid">
-              <img class="product-image"
-                src="${image}">
-
-              <div class="cart-item-details">
-                <div class="product-name">
-                  "${name}"
+    products.forEach((product) => {
+      if (product.id === cartItem.id) {
+        const { image, name, priceCents } = product;
+  
+        cartGenerated +=`<div class="cart-item-container js-cart-item-container-${product.id}">
+              <div class="delivery-date js-delivery-date">
+                Delivery date: ${cartItem.deliveryOptionId ? convertDeliveryOptions(cartItem.deliveryOptionId) : ''}
+              </div>
+  
+              <div class="cart-item-details-grid">
+                <img class="product-image"
+                  src="${image}">
+  
+                <div class="cart-item-details">
+                  <div class="product-name">
+                    "${name}"
+                  </div>
+                  <div class="product-price">
+                    $${formatCurrency(priceCents)}
+                  </div>
+                  <div class="product-quantity">
+                    <span>
+                      Quantity: <span class="quantity-label">${cartItem.quantity}</span>
+                    </span>
+                    <span class="update-quantity-link link-primary">
+                      Update
+                    </span>
+                    <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${cartItem.id}">
+                      Delete
+                    </span>
+                  </div>
                 </div>
-                <div class="product-price">
-                  $${formatCurrency(priceCents)}
-                </div>
-                <div class="product-quantity">
-                  <span>
-                    Quantity: <span class="quantity-label">${cartItem.quantity}</span>
-                  </span>
-                  <span class="update-quantity-link link-primary">
-                    Update
-                  </span>
-                  <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${cartItem.id}">
-                    Delete
-                  </span>
+  
+                <div class="delivery-options">
+                  <div class="delivery-options-title">
+                    Choose a delivery option:
+                  </div>
+                  ${generateDeliveryOptions(cartItem, index)}
                 </div>
               </div>
-
-              <div class="delivery-options">
-                <div class="delivery-options-title">
-                  Choose a delivery option:
-                </div>
-                ${generateDeliveryOptions(cartItem, index)}
-              </div>
             </div>
-          </div>
-        `;
-    }
+          `;
+      }
+    });
   });
-});
 
-document.querySelector('.js-order-summary').innerHTML = cartGenerated;
+  document.querySelector('.js-order-summary').innerHTML = cartGenerated;
+
+  document.querySelectorAll('.js-delivery-option-input').forEach((deliveryOption) => {
+    deliveryOption.addEventListener('click', () => {
+      cart.forEach((cartItem, index) => {
+        if(cartItem.id === deliveryOption.dataset.productId) {
+          cartItem.deliveryOptionId = deliveryOption.dataset.deliveryOption;
+          // document.querySelectorAll('.js-delivery-date')[index].innerHTML = `Delivery date: ${convertDeliveryOptions(deliveryOption.dataset.deliveryOption)}`;
+          saveToStorage(cart);
+          renderCheckOutPage();
+        }
+      });
+    });
+  });
+
+  document.querySelectorAll('.js-delete-link').forEach((link) => {
+    link.addEventListener('click', () => {
+      removeFromCart(link.dataset.productId);
+    });
+  });
+
+  document.querySelector('.js-payment-summary-row')
+  .innerHTML = `Items (${getCartQuantity(cart)}):`;
+
+  document.querySelector('.js-return-to-home-link')
+    .innerHTML = `${getCartQuantity(cart)} Items`;
+}
+
+renderCheckOutPage();
 
 function generateDeliveryOptions(cartItem, index) {
   deliveryOptionsHtml = '';
@@ -91,27 +120,3 @@ function generateDeliveryOptions(cartItem, index) {
 
   return deliveryOptionsHtml;
 }
-
-document.querySelectorAll('.js-delivery-option-input').forEach((deliveryOption) => {
-  deliveryOption.addEventListener('click', () => {
-    cart.forEach((cartItem, index) => {
-      if(cartItem.id === deliveryOption.dataset.productId) {
-        cartItem.deliveryOptionId = deliveryOption.dataset.deliveryOption;
-        document.querySelectorAll('.js-delivery-date')[index].innerHTML = `Delivery date: ${convertDeliveryOptions(deliveryOption.dataset.deliveryOption)}`;
-        saveToStorage(cart);
-      }
-    });
-  });
-});
-
-document.querySelectorAll('.js-delete-link').forEach((link) => {
-  link.addEventListener('click', () => {
-    removeFromCart(link.dataset.productId);
-  });
-});
-
-document.querySelector('.js-payment-summary-row')
-  .innerHTML = `Items (${getCartQuantity(cart)}):`;
-
-document.querySelector('.js-return-to-home-link')
-  .innerHTML = `${getCartQuantity(cart)} Items`;
